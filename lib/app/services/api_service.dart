d47782a1dc9acde152b14bfca445c2e3bc539cf1
bizz84 <bizz84@gmail.com>
2020-03-05 12:30:25 +0000

    Parse the access token

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 
@@ -10,5 +12,15 @@ class APIService {
       api.tokenUri().toString(),
       headers: {'Authorization': 'Basic ${api.apiKey}'},
     );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final accessToken = data['access_token'];
+      if (accessToken != null) {
+        return accessToken;
+      }
+    }
+    print(
+        'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
   }
 }
