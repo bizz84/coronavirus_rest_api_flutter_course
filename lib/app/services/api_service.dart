c7c24b7dc922f9a6d086b84665063adeef89ad92
bizz84 <bizz84@gmail.com>
2020-03-05 17:07:30 +0000

    Add the getEndpointData method

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 
@@ -23,4 +24,37 @@ class APIService {
         'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
     throw response;
   }
+
+  Future<int> getEndpointData({
+    @required String accessToken,
+    @required Endpoint endpoint,
+  }) async {
+    final uri = api.endpointUri(endpoint);
+    final response = await http.get(
+      uri.toString(),
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint];
+        final int result = endpointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+    print(
+        'Request $uri failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
+  }
+
+  static Map<Endpoint, String> _responseJsonKeys = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'data',
+    Endpoint.casesConfirmed: 'data',
+    Endpoint.deaths: 'data',
+    Endpoint.recovered: 'data',
+  };
 }
