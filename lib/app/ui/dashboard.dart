52f07a35fbe6015e1cb6f93c04a9be8f9b2d1721
bizz84 <bizz84@gmail.com>
2020-03-10 15:11:38 +0000

    Add Dashboard widget

---
+++ lib/app/ui/dashboard.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Coronavirus Tracker'),
+      ),
+      body: ListView(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
