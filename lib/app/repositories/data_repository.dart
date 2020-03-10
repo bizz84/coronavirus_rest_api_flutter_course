c267954b7a18ccda132eeb280703501e6b556760
bizz84 <bizz84@gmail.com>
2020-03-10 14:16:24 +0000

    Refresh access token in DataRepository

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,14 +1,29 @@
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
 
 class DataRepository {
   DataRepository({@required this.apiService});
   final APIService apiService;
 
+  String _accessToken;
+
   Future<int> getEndpointData(Endpoint endpoint) async {
-    final accessToken = await apiService.getAccessToken();
-    return await apiService.getEndpointData(
-        accessToken: accessToken, endpoint: endpoint);
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: endpoint);
+    } on Response catch (response) {
+      // if unauthorized, get access token again
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndpointData(
+            accessToken: _accessToken, endpoint: endpoint);
+      }
+      rethrow;
+    }
   }
 }
