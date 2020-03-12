6fc2080e89e3db37c7d42367849bf7c102038954
bizz84 <bizz84@gmail.com>
2020-03-12 09:07:13 +0000

    Implement the EndpointCard

--- lib/app/ui/endpoint_card.dart
+++ lib/app/ui/endpoint_card.dart
@@ -1,8 +1,33 @@
+import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:flutter/material.dart';
 
 class EndpointCard extends StatelessWidget {
+  const EndpointCard({Key key, this.endpoint, this.value}) : super(key: key);
+  final Endpoint endpoint;
+  final int value;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Cases',
+                style: Theme.of(context).textTheme.headline,
+              ),
+              Text(
+                value != null ? value.toString() : '',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
