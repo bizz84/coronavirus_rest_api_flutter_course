c896c00bbc4f2e3d2d0d8414d7634000bd3b2f45
bizz84 <bizz84@gmail.com>
2020-03-18 15:17:01 +0000

    Add _getAllEndpointsData() method

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -26,4 +26,19 @@ class DataRepository {
       rethrow;
     }
   }
+
+  Future<void> _getAllEndpointsData() async {
+    await Future.wait([
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.cases),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesSuspected),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesConfirmed),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.deaths),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.recovered),
+    ]);
+  }
 }
