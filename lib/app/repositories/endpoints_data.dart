fd704330969933e413905b6a1ec0c20949ada2e2
bizz84 <bizz84@gmail.com>
2020-03-27 14:33:02 +0000

    Add the EndpointData class

--- lib/app/repositories/endpoints_data.dart
+++ lib/app/repositories/endpoints_data.dart
@@ -1,14 +1,15 @@
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/services/endpoint_data.dart';
 import 'package:flutter/foundation.dart';
 
 class EndpointsData {
   EndpointsData({@required this.values});
-  final Map<Endpoint, int> values;
-  int get cases => values[Endpoint.cases];
-  int get casesSuspected => values[Endpoint.casesSuspected];
-  int get casesConfirmed => values[Endpoint.casesConfirmed];
-  int get deaths => values[Endpoint.deaths];
-  int get recovered => values[Endpoint.recovered];
+  final Map<Endpoint, EndpointData> values;
+  EndpointData get cases => values[Endpoint.cases];
+  EndpointData get casesSuspected => values[Endpoint.casesSuspected];
+  EndpointData get casesConfirmed => values[Endpoint.casesConfirmed];
+  EndpointData get deaths => values[Endpoint.deaths];
+  EndpointData get recovered => values[Endpoint.recovered];
 
   @override
   String toString() =>
