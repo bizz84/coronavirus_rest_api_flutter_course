6cb1dd0e69c3214ca859edb84a3d675f6e612852
bizz84 <bizz84@gmail.com>
2020-05-09 20:49:51 +0100

    Add a LastUpdatedDateFormatter using the intl package

--- lib/app/ui/last_updated_status_text.dart
+++ lib/app/ui/last_updated_status_text.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class LastUpdatedDateFormatter {
+  LastUpdatedDateFormatter({@required this.lastUpdated});
+  final DateTime lastUpdated;
+
+  String lastUpdatedStatusText() {
+    if (lastUpdated != null) {
+      final formatter = DateFormat.yMd().add_Hms();
+      final formatted = formatter.format(lastUpdated);
+      return 'Last updated: $formatted';
+    }
+    return '';
+  }
+}
 
 class LastUpdatedStatusText extends StatelessWidget {
   const LastUpdatedStatusText({Key key, @required this.text}) : super(key: key);
