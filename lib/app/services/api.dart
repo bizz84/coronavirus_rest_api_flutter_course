34dc3a59c33f2c61f1ed3e6dda646aad02910d5a
Andrea Bizzotto <bizz84@gmail.com>
2020-07-01 10:18:02 +0100

    Add endpointUrl method to API class

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,6 +1,14 @@
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_keys.dart';
 import 'package:flutter/foundation.dart';
 
+enum Endpoint {
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
 class API {
   API({@required this.apiKey});
   final String apiKey;
@@ -14,4 +22,18 @@ class API {
         host: host,
         path: 'token',
       );
+
+  Uri endpointUri(Endpoint endpoint) => Uri(
+        scheme: 'https',
+        host: host,
+        path: _paths[endpoint],
+      );
+
+  static Map<Endpoint, String> _paths = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'casesSuspected',
+    Endpoint.casesConfirmed: 'casesConfirmed',
+    Endpoint.deaths: 'deaths',
+    Endpoint.recovered: 'recovered',
+  };
 }
