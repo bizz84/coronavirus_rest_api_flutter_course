fd8a2d50e9e079953de58b54482446608409b918
Andrea Bizzotto <bizz84@gmail.com>
2020-07-01 11:13:30 +0100

    Add Dashboard widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:coronavirus_rest_api_flutter_course/app/repositories/data_repository.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/ui/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -21,112 +22,8 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: Color(0xFF101010),
           cardColor: Color(0xFF222222),
         ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
+        home: Dashboard(),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _accessToken = '';
-  int _cases;
-  int _deaths;
-
-  void _updateAccessToken() async {
-    final apiService = APIService(API.sandbox());
-    final accessToken = await apiService.getAccessToken();
-    final cases = await apiService.getEndpointData(
-      accessToken: accessToken,
-      endpoint: Endpoint.cases,
-    );
-    final deaths = await apiService.getEndpointData(
-      accessToken: accessToken,
-      endpoint: Endpoint.deaths,
-    );
-    setState(() {
-      _accessToken = accessToken;
-      _cases = cases;
-      _deaths = deaths;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_accessToken',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            if (_cases != null)
-              Text(
-                'cases: $_cases',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            if (_deaths != null)
-              Text(
-                'deaths: $_deaths',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _updateAccessToken,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
