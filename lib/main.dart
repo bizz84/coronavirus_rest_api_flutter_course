b876910bfb37806d8603c981d3dd5b018834421d
Andrea Bizzotto <bizz84@gmail.com>
2020-07-01 11:20:23 +0100

    Add data cache service to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
 import 'package:coronavirus_rest_api_flutter_course/app/repositories/data_repository.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/services/data_cache_service.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/ui/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Intl.defaultLocale = 'en_GB';
   await initializeDateFormatting();
-  runApp(MyApp());
+  final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(MyApp(sharedPreferences: sharedPreferences));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key, @required this.sharedPreferences}) : super(key: key);
+  final SharedPreferences sharedPreferences;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Provider<DataRepository>(
       create: (_) => DataRepository(
         apiService: APIService(API.sandbox()),
+        dataCacheService: DataCacheService(
+          sharedPreferences: sharedPreferences,
+        ),
       ),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
